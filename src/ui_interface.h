8618525e213012bc7c7e763acc3d4a68b2b1bfe4
jamisoncherry0104-art <jamisoncherry0104@gmail.com>
2025-09-10 20:08:58 +0800

    add boost bind

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -9,6 +9,8 @@
 #include "util.h" // for int64
 #include <boost/signals2/signal.hpp>
 #include <boost/signals2/last_value.hpp>
+#include <boost/bind/bind.hpp>
+using namespace boost::placeholders;
 
 class CBasicKeyStore;
 class CWallet;
